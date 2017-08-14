b8c1a6fae9bab0386f63a511ce54d8f562327542
Antonio Chambers <r.antonio.chambers@gmail.com>
2017-08-14 16:07:53 -0400

    seconds and days fixed

--- deeandtee.js
+++ deeandtee.js
@@ -7,11 +7,13 @@ console.log(chalk.blue('Hello world!'));
 console.log(`It is ${chalk.blue(moment().format())}`);
 
 let currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
-let currentDayth = moment().format();
-let currentSeconds = moment().format();
+let currentDayth = moment().format("DDDo");
+let currentSeconds = moment().format("s");
 let isDaylightSavingsTime = moment().isDST();
 let frogYear = moment().isLeapYear();
 
 console.log(`It is ${chalk.blue(currentDate)}`);
-console.log(`It ${chalk.green(isDaylightSavingsTime)}`);
-console.log(`It ${chalk.red(frogYear)}`);
+console.log(`It is the ${chalk.magenta(currentDayth)} of the year`);
+console.log(`It is ${chalk.cyan(currentSeconds)} seconds into the day`);
+console.log(`It is ${chalk.green(isDaylightSavingsTime)} that it is Daylight Savings Time`);
+console.log(`It is ${chalk.red(frogYear)} that is is a Leap Year`);
